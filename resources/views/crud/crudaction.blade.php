72868529bde205011c7f5b28de3472ff9f068429
Akshay <akumarraheja@gmail.com>
2022-06-12 02:17:04 +0530

    updates

--- resources/views/crud/crudaction.blade.php
+++ resources/views/crud/crudaction.blade.php
@@ -1,6 +1,6 @@
 
 <a class="btn btn-sm btn-primary mr-1" href="crud/{{$slug}}/edit"><i class="mdi mdi-pencil"></i></a>
-<form action='{{admin_url("crud/$slug")}}' method="POST" style="display: inline">
+<form action='{{admin_url("crud/$slug")}}' method="POST" style="display: inline" pjax-container>
     @method('delete')
     @csrf
     <button type="submit" class="btn btn-sm btn-danger"><i class="mdi mdi-delete"></i></button>
