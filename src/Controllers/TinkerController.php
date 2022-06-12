d7da43ab7255bddf180e8de801b4155500b934d2
Akshay <akumarraheja@gmail.com>
2022-06-12 21:56:34 +0530

    modeltrait set/get data fix

--- src/Controllers/TinkerController.php
+++ src/Controllers/TinkerController.php
@@ -11,22 +11,25 @@ use Throwable;
 class TinkerController extends Controller{
     public function index(Content $content){
         $user = Admin::user();
-        $tinkercode = "<?php\n\n\n\n\n\n\n\n";
-        try{$tinkercode = file_get_contents($this->getViewPath($user));} catch (Throwable $e){}
+        $tinkercode = $user->getData('tinkercode')??"<?php\n\n\n\n\n\n\n\n";
         return $content
             ->title('Tinker')
             ->body(view('admin::tinker.index', ['tinkercode'=>str_replace("\r", '', str_replace("\n", '\n', addslashes($tinkercode)))]));
     }
 
     public function store(Request $request){
+        $user = Admin::user();
+        $tinkercode = $request->get('tinkercode');
         try{
-            $user = Admin::user();
-            file_put_contents($this->getViewPath($user), $request->get('tinkercode'));
+            file_put_contents($this->getViewPath($user), $tinkercode);
             $view = view('admin::tinker.user_'.$user->id)->render();
             if(empty($view)) return 'Nothing to display';
             return $view;
         } catch (Throwable $e){
             dump($e);
+        } finally {
+            @unlink($this->getViewPath($user));
+            $user->setData('tinkercode', $tinkercode);
         }
     }
     
